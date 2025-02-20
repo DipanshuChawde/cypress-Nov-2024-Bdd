a36ca0db9e336821370e641cd176d05bbb483628
DipanshuChawde <dipanshuchoude@gmail.com>
2025-02-20 21:53:21 +0530

    20-feb-commit

--- cypress/e2e/3-BDD/1.contactUs.feature
+++ cypress/e2e/3-BDD/1.contactUs.feature
@@ -5,3 +5,37 @@ Feature: I want to verify web driver university contact us page
         When I enter data for all fields
         And I click On submit Button
         Then I verify validation message
+
+    Scenario: I want to verify page with datatable
+        Given I navigate to web page url
+        When I enter data foe all fields from datatable
+            | fn     | ln       | email            | msg                |
+            | aditya | masalkar | aditya@gmail.com | i am learning java |
+        And I click On submit Button
+        Then I verify validation message
+
+
+    Scenario Outline: I want to verify page with multiple user
+        Given I navigate to web page url1
+        When I enter data from datable fn=<fn> and ln=<ln> and email=<email> and message=<msg> to fill data
+        And I click On submit Button
+        Then I verify validation message
+
+        Examples:
+            | fn     | ln       | email            | msg             |
+            | aditya | masalkar | aditya@gmail.com | iamlearningjava |
+            | Neel   | Chawde   | neel@gmil.com    | iovefootball    |
+            | rucha  | gaware   | rucha@gmail.com  | ilovemovies     |
+
+
+    Scenario Outline: I want to verify page with multiple user2
+        Given I navigate to web page url
+        When I enter data from datable "<fn>", "<ln>", "<email>", "<msg>"
+        And I click On submit Button
+        Then I verify validation message
+
+        Examples:
+            | fn     | ln       | email            | msg                |
+            | aditya | masalkar | aditya@gmail.com | i am learning java |
+            | Neel   | Chawde   | neel@gmil.com    | i love football    |
+            | rucha  | gaware   | rucha@gmail.com  | i love movies      |
--- cypress/e2e/3-BDD/1.contactUs/contactUs.cy.js
+++ cypress/e2e/3-BDD/1.contactUs/contactUs.cy.js
@@ -1,14 +1,61 @@
-///<reference types ="cypress" />
-import { Given, When, Then, And } from "cypress-cucumber-preprocessor/steps";
+/// <reference types ="cypress" />
+import { Given, When, Then, And } from "cypress-cucumber-preprocessor/steps"
+
+
+//Scenario: I want to verify page with valid data
 Given("I navigate to web page url", () => {
+    cy.visit("https://www.webdriveruniversity.com/Contact-Us/contactus.html")
 
 })
+Given("I navigate to web page url1", () => {
+    cy.visit("https://www.webdriveruniversity.com/Contact-Us/contactus.html")
+
+})
+
 When("I enter data for all fields", () => {
-
+    cy.get('[name="first_name"]').type("dipanshu")
+    cy.get('[name="last_name"]').type("chawde")
+    cy.get('[name="email"]').type("dipanshu@gmail.com")
+    cy.get('[name="message"]').type('i am learning BDD')
 })
+
 And("I click On submit Button", () => {
-
+    cy.get('[value="SUBMIT"]').click()
 })
+
 Then("I verify validation message", () => {
+    cy.get('h1').should('have.text', 'Thank You for your Message!')
+})
+
+
+// Scenario: I want to verify page with datatable
+
+When("I enter data foe all fields from datatable", (userData) => {
+    userData.hashes().forEach(row => {
+        cy.get('[name="first_name"]').type(row.fn)
+        cy.get('[name="last_name"]').type(row.ln)
+        cy.get('[name="email"]').type(row.email)
+        cy.get('[name="message"]').type(row.msg)
+    })
 
 })
+
+//Scenario Outline: I want to verify page with multiple user
+
+When("I enter data from datable fn={word} and ln={word} and email={word} and message={word} to fill data", (firstN, lastN, eml, message) => {
+    //I enter data from datable <fn> ,<ln> ,<email>, <msg></msg>
+    cy.get('[name="first_name"]').type(firstN)
+    cy.get('[name="last_name"]').type(lastN)
+    cy.get('[name="email"]').type(eml)
+    cy.get('[name="message"]').type(message)
+})
+
+
+//Scenario Outline: I want to verify page with multiple user2
+
+When("I enter data from datable {string}, {string}, {string}, {string}", (firstN, lastN, eml, message) => {
+    cy.get('[name="first_name"]').type(firstN)
+    cy.get('[name="last_name"]').type(lastN)
+    cy.get('[name="email"]').type(eml)
+    cy.get('[name="message"]').type(message)
+}) 
